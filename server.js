9bba40f81c4c3f9685b72e2ac8ed062ea00fc058
Hesham <heshammaher@outlook.com>
2024-09-14 06:20:51 +0300

    all files

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-const Butter = require("./butter");
+const http = require("node:http");
 
-const server = new Butter();
+const server = http.createServer((req, res) => {
+  console.log("------ METHODS ------");
+  // For http/1.1 method are in separate field inside request
+  console.log(req.method);
 
-const PORT = 4060;
+  console.log("------ HEADERS ------");
+  console.log(req.headers);
 
-server.route("get", "/", (req, res) => {
-  res.sendFile("./public/index.html", "text/html");
+  console.log("------ URL ------");
+  console.log(req.url);
+
+  console.log("------ BODY ------");
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk.toString("utf-8");
+    console.log(data);
+  });
+
+  req.on("end", () => {
+    data = JSON.parse(data);
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        //@ts-ignore
+        message: `Post with title: ${data.title} was created by user with id: ${data.userId}`,
+      })
+    );
+  });
 });
 
-server.route("get", "/style.css", (req, res) => {
-  res.sendFile("./public/style.css", "text/css");
-});
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
+server.listen("8000", () => {
+  console.log("Server listening on http://localhost:8000");
 });
