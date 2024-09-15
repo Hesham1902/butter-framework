7e97d912015d06f153d4dd542e180b748a6861fd
Hesham <heshammaher@outlook.com>
2024-09-15 03:10:10 +0300

    only poster project included

--- server.js
+++ server.js
@@ -1,36 +1,99 @@
-const http = require("node:http");
+//@ts-nocheck
+const Butter = require("../butter");
 
-const server = http.createServer((req, res) => {
-  console.log("------ METHODS ------");
-  // For http/1.1 method are in separate field inside request
-  console.log(req.method);
+const USERS = [
+  {
+    id: 1,
+    name: "John Doe",
+    username: "john",
+    password: "string",
+  },
+  {
+    id: 2,
+    name: "Jane smith",
+    username: "jane",
+    password: "string",
+  },
+  {
+    id: 3,
+    name: "Jim fisher",
+    username: "jim",
+    password: "string",
+  },
+];
 
-  console.log("------ HEADERS ------");
-  console.log(req.headers);
+const POSTS = [
+  {
+    id: 1,
+    title: "Post 1",
+    body: "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    userId: 1,
+  },
+];
 
-  console.log("------ URL ------");
-  console.log(req.url);
+const server = new Butter();
 
-  console.log("------ BODY ------");
-  let data = "";
-  req.on("data", (chunk) => {
-    data += chunk.toString("utf-8");
-    console.log(data);
+const PORT = 8000;
+
+// ------- FILES ROUTES --------
+server.route("get", "/", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+server.route("get", "/styles.css", (req, res) => {
+  res.sendFile("./public/styles.css", "text/css");
+});
+
+server.route("get", "/scripts.js", (req, res) => {
+  res.sendFile("./public/scripts.js", "text/javascript");
+});
+
+server.route("get", "/login", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
+// ------- JSON ROUTES --------
+server.route("get", "/api/posts", (req, res) => {
+  const posts = POSTS.map((post) => {
+    const user = USERS.find((user) => user.id === post.userId);
+    post.author = user?.name;
+    return post;
   });
 
-  req.on("end", () => {
-    data = JSON.parse(data);
+  res.status(200).json(posts);
+});
 
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        //@ts-ignore
-        message: `Post with title: ${data.title} was created by user with id: ${data.userId}`,
-      })
+server.route("post", "/api/login", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString("utf-8");
+    console.log(chunk.toString("utf-8"));
+  });
+  req.on("end", () => {
+    body = JSON.parse(body);
+
+    const user = USERS.find(
+      (user) =>
+        user.username === body.username && user.password === body.password
     );
+
+    if (!user) {
+      res.status(401).json({ message: "Invalid credentials" });
+    } else {
+      loggedUser = user;
+      res.status(200).json({ message: "Logged in successfully" });
+    }
   });
 });
 
-server.listen("8000", () => {
-  console.log("Server listening on http://localhost:8000");
+server.route("get", "/api/user", (req, res) => {
+  res.status(200).json(loggedUser);
+});
+
+server.route("delete", "/api/logout", (req, res) => {
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 });
